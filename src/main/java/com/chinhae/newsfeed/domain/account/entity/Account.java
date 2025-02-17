75213027fc2c2c2b38d41c2b3349be6f8c13b695
김종연 <bellopenerr@gmail.com>
2025-02-17 18:49:44 +0900

    chore: base entity 위치 변경

--- src/main/java/com/chinhae/newsfeed/domain/account/entity/Account.java
+++ src/main/java/com/chinhae/newsfeed/domain/account/entity/Account.java
@@ -1,7 +1,13 @@
 package com.chinhae.newsfeed.domain.account.entity;
 
-import com.chinhae.newsfeed.baseEntity.BaseEntity;
-import jakarta.persistence.*;
+import com.chinhae.newsfeed.domain.base.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -16,17 +22,17 @@ public class Account extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, length = 255)
-    private String email; // 이메일
+  @Column(nullable = false, length = 255)
+  private String email; // 이메일
 
-    @Column(nullable = false, length = 80)
-    private String password; // 비밀번호
+  @Column(nullable = false, length = 80)
+  private String password; // 비밀번호
 
-    @Column(nullable = false, length = 20) // 한글 기준 최대 6자
-    private String username; // 사용자 이름
+  @Column(nullable = false, length = 20) // 한글 기준 최대 6자
+  private String username; // 사용자 이름
 
-    @Column
-    private LocalDate birthDate; // 생년월일
+  @Column
+  private LocalDate birthDate; // 생년월일
 
     public Account(String email, String password, String username, LocalDate birthDate) {
         this.email = email;
---
+++ src/main/java/com/chinhae/newsfeed/domain/base/BaseEntity.java
@@ -0,0 +1,27 @@
+package com.chinhae.newsfeed.domain.base;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+  @CreatedDate
+  @Column(updatable = false)
+  @Temporal(TemporalType.TIMESTAMP)
+  private LocalDateTime created_at;
+
+  @LastModifiedDate
+  @Temporal(TemporalType.TIMESTAMP)
+  private LocalDateTime updated_at;
+}
