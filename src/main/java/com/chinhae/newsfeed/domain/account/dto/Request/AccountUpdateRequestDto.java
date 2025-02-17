3e9f62c56630ef837125286554eec897ac39ec92
dawon <aodlstory321@gmail.com>
2025-02-17 21:57:15 +0900

    feat:회원가입 Vaild 구현

--- src/main/java/com/chinhae/newsfeed/domain/account/dto/Request/AccountUpdateRequestDto.java
+++ src/main/java/com/chinhae/newsfeed/domain/account/dto/Request/AccountUpdateRequestDto.java
@@ -1,9 +1,12 @@
 package com.chinhae.newsfeed.domain.account.dto.Request;
 
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 
 @Getter
 public class AccountUpdateRequestDto {
+
     private String currentPassword;
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*\\W)[^\\s]{8,16}$", message = "비밀번호는 8~16자, 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
     private String newPassword;
 }
