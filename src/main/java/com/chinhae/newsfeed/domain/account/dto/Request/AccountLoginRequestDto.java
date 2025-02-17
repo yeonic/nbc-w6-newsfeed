7d3a43362062d5e43fb56aeb3f6cafd933bb147a
dawon <aodlstory321@gmail.com>
2025-02-17 21:57:15 +0900

    feat:회원가입 Vaild 구현

--- src/main/java/com/chinhae/newsfeed/domain/account/dto/Request/AccountLoginRequestDto.java
+++ src/main/java/com/chinhae/newsfeed/domain/account/dto/Request/AccountLoginRequestDto.java
@@ -1,10 +1,13 @@
 package com.chinhae.newsfeed.domain.account.dto.Request;
 
+import jakarta.validation.constraints.Email;
 import lombok.Getter;
 
 @Getter
 public class AccountLoginRequestDto {
 
+    @Email
     private String email;
+
     private String password;
 }
--- src/main/java/com/chinhae/newsfeed/domain/account/dto/Request/AccountSignupRequestDto.java
+++ src/main/java/com/chinhae/newsfeed/domain/account/dto/Request/AccountSignupRequestDto.java
@@ -1,13 +1,18 @@
 package com.chinhae.newsfeed.domain.account.dto.Request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
+import org.hibernate.validator.constraints.Range;
 
 import java.time.LocalDate;
 
 @Getter
 public class AccountSignupRequestDto {
 
+    @Email
     private String email;
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*\\W)[^\\s]{8,16}$", message = "비밀번호는 8~16자, 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
     private String password;
     private String username;
     private LocalDate birthDate;
