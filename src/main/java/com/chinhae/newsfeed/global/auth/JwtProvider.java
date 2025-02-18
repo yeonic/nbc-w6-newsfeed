2ac5222a0450f19dce0392460744a739ed261bfb
dawon <aodlstory321@gmail.com>
2025-02-18 17:49:24 +0900

    feat:JWT를 활용한 로그인 기능 구현

---
+++ src/main/java/com/chinhae/newsfeed/global/auth/JwtProvider.java
@@ -0,0 +1,46 @@
+package com.chinhae.newsfeed.global.auth;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtProvider {
+    public static final byte[] secret = "ChoiDawonSecretKeyChoiDawonSecretKeyChoiDawonSecretKey".getBytes();
+    private final Key key = Keys.hmacShaKeyFor(secret);
+
+    public String createToken(Map<String, Object> claims, Date expireDate) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setExpiration(expireDate)
+                .signWith(key)
+                .compact();
+    }
+
+    public Claims getClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public Jwt createJwt(Map<String, Object> claims) {
+        String accessToken = createToken(claims, getExpireDateAccessToken());
+        return Jwt.builder()
+                .accessToken(accessToken)
+                .build();
+    }
+
+    public Date getExpireDateAccessToken() {
+        long expireTimeMils = 1000 * 60 * 60;
+        return new Date(System.currentTimeMillis() + expireTimeMils);
+    }
+}
