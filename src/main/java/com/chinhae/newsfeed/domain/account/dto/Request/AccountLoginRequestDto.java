860a7e9b8d95a94633fbd8595b67a9e0504f02de
dawon <aodlstory321@gmail.com>
2025-02-18 14:05:32 +0900

    chore: Validation 에러를 상추로 처리, ddl-auto 제거

--- src/main/java/com/chinhae/newsfeed/domain/account/dto/Request/AccountLoginRequestDto.java
+++ src/main/java/com/chinhae/newsfeed/domain/account/dto/Request/AccountLoginRequestDto.java
@@ -1,5 +1,6 @@
 package com.chinhae.newsfeed.domain.account.dto.Request;
 
+import com.chinhae.newsfeed.global.messages.ValidationConst;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
@@ -7,8 +8,7 @@ import lombok.Getter;
 @Getter
 public class AccountLoginRequestDto {
 
-    @Pattern(regexp = "^^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$", message = "이메일을 입력하세요.")
+    @Pattern(regexp = "^^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$", message = ValidationConst.EMAIL_IS_WRONG)
     private String email;
-
     private String password;
 }
