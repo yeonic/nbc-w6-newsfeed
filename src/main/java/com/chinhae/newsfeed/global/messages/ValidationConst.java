d0f2cbe84c4f94a8bfdfa994362faf934a7b5877
dawon <aodlstory321@gmail.com>
2025-02-18 14:05:32 +0900

    chore: Validation 에러를 상추로 처리, ddl-auto 제거

---
+++ src/main/java/com/chinhae/newsfeed/global/messages/ValidationConst.java
@@ -0,0 +1,6 @@
+package com.chinhae.newsfeed.global.messages;
+
+public abstract class ValidationConst {
+    public static final String WRONG_PASSWORD = "비밀번호는 8~16자, 영문 대 소문자, 숫자, 특수문자를 사용하세요.";
+    public static final String EMAIL_IS_WRONG = "이메일을 입력해주세요.";
+}
