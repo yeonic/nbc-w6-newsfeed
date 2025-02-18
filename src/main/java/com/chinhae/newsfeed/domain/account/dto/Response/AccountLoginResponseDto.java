105d467f4971ae45f81fb1fbc803b7475651e2d8
dawon <aodlstory321@gmail.com>
2025-02-18 11:33:28 +0900

    feat:회원조회시에 탈퇴한 회원은 조회가 안되게 변경

--- src/main/java/com/chinhae/newsfeed/domain/account/dto/Response/AccountLoginResponseDto.java
+++ src/main/java/com/chinhae/newsfeed/domain/account/dto/Response/AccountLoginResponseDto.java
@@ -10,6 +10,9 @@ public class AccountLoginResponseDto {
     private final String email;
     private final String username;
     private final LocalDateTime createAt;
+    // 거기에서 받은 엔티티를 세션키에 넣어라.
+    // 로그인된 계정하고 프로필도 있어야한다.
+    // 로그인을 할때 프로필도 담겨야한다.
 
     public AccountLoginResponseDto(String email, String username, LocalDateTime createAt) {
         this.email = email;
