841ec96c100fbffbb289b5bc82a04cc3c6fc2c9c
김종연 <bellopenerr@gmail.com>
2025-02-19 17:03:07 +0900

    fix: session 받아와지지 않는 문제 해결

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+insert into profile (id, account_id, nickname, profile_img_url, bio, friends_count, posts_count, created_at, updated_at) values (9999, null, '테스트 유저1', '', '', 0, 0, NOW(), NOW())
+insert into profile (id, account_id, nickname, profile_img_url, bio, friends_count, posts_count, created_at, updated_at) values (99999, null, '테스트 유저2', '', '', 0, 0, NOW(), NOW())
+insert into profile (id, account_id, nickname, profile_img_url, bio, friends_count, posts_count, created_at, updated_at) values (999999, null, '테스트 유저3', '', '', 0, 0, NOW(), NOW())
+
