7b2d291c5ccff16055e2c398d597eef30256fa4f
김종연 <bellopenerr@gmail.com>
2025-02-20 02:28:16 +0900

    chore: undo merge manually PR#16

--- src/main/java/com/chinhae/newsfeed/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/chinhae/newsfeed/domain/comment/repository/CommentRepository.java
@@ -2,24 +2,19 @@ package com.chinhae.newsfeed.domain.comment.repository;
 
 import com.chinhae.newsfeed.domain.comment.dto.CommentCountDto;
 import com.chinhae.newsfeed.domain.comment.entity.Comment;
-import com.chinhae.newsfeed.domain.post.entity.Post;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     List<Comment> findByPost_Id(Long postId);
 
-    @Query("select new com.chinhae.newsfeed.domain.comment.dto.CommentCountDto(c.post.id, count(c)) " +
+    @Query(
+        "select new com.chinhae.newsfeed.domain.comment.dto.CommentCountDto(c.post.id, count(c)) " +
             "from Comment c " +
             "where c.post.id in :posts " +
             "group by c.post.id"
-            )
+    )
     List<CommentCountDto> countByPostIds(List<Long> posts);
-
-    Long countCommentByPost_Id(Long postId);
-
-    List<Long> post(Post post);
 }
